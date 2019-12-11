44044b1b2389c9acf745b52d9afd55d55b5e1e75
Aaron Gustafson <aaron@easy-designs.net>
2019-12-11 12:22:45 -0800

    Initial checkin

---
+++ sw.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const staticCacheName = 'staticfiles';
+
+self.addEventListener('install', installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticCacheName)
+    .then( staticCache => {
+      return staticCache.addAll([
+        '/j/main.js',
+        '/c/default.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return fetch(fetchEvent.request)
+              .then(function(response) {
+                cache.put(fetchEvent.request, response.clone());
+                return response;
+              })
+              .catch(function() {
+                return caches.match(fetchEvent.request);
+              });
+    })
+  ); // end respondWith
+}); // end addEventListener
